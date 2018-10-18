4307f8bed3d2c1ba283e0e3e7570a6ec9bda7b9a
bilalbutt044 <bilal.soft000@gmail.com>
2018-10-18 22:13:03 +0500

    Data Binding

--- Angularapp/src/app/shared/dish.ts
+++ Angularapp/src/app/shared/dish.ts
@@ -1,9 +1,13 @@
+import { Comment } from './comments';
 
 export class Dish {
+    id: string;
     name: string;
     image: string;
     category: string;
+    featured: boolean;
     label: string;
     price: string;
     description: string;
+    comments: Comment[];
 }
