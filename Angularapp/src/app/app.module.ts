f9941e7e964d2bf521b7d39466cb02b0057fc718
bilalbutt044 <bilal.soft000@gmail.com>
2018-11-02 19:11:39 +0500

    single page application

--- Angularapp/src/app/app.module.ts
+++ Angularapp/src/app/app.module.ts
@@ -22,6 +22,7 @@ import { FooterComponent } from './footer/footer.component';
 import { DishService } from './services/dish.service';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { PromotionService } from './services/promotion.service';
 
 
 @NgModule({
@@ -46,7 +47,7 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     MatButtonModule,
     AppRoutingModule
   ],
-  providers: [DishService],
+  providers: [DishService, PromotionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- Angularapp/src/app/dishdetail/dishdetail.component.ts
+++ Angularapp/src/app/dishdetail/dishdetail.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 import { Dish } from '../shared/dish';
+import { DishService } from '../services/dish.service';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-dishdetail',
@@ -8,10 +11,19 @@ import { Dish } from '../shared/dish';
   styleUrls: ['./dishdetail.component.css']
 })
 export class DishdetailComponent implements OnInit {
-  @Input() dish = Dish;
-  constructor() { }
+  dish: Dish;
+  constructor(
+    private dishService: DishService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) { }
 
   ngOnInit() {
+    let id = this.route.snapshot.params['id'];
+    this.dish = this.dishService.getDish(id);
   }
 
+  goBack(): void {
+    this.location.back();
+  }
 }
